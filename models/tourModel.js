193aa39c41d21768408feec198038a0bcbead4e9
ducduy0209 <ducduy0209.work@gmail.com>
2022-05-13 22:36:39 +0700

    commit on May 13th

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const slugify = require('slugify')
 const tourSchema = mongoose.Schema(
   {
     name: {
@@ -8,6 +8,7 @@ const tourSchema = mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +64,16 @@ const tourSchema = mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return (this.duration / 7).toFixed(2)
 })
+
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc)
+//   next()
+// })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
