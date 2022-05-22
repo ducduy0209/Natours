0a8efa8ad3ebec6d7454d1c6bb00c14892220fa3
ducduy0209 <ducduy0209.work@gmail.com>
2022-05-23 00:06:38 +0700

    It is the end of Error setion

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION!')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 const app = require('./app')
 
 const DB = process.env.DATABASE.replace(
@@ -21,6 +27,14 @@ mongoose
   })
 
 const PORT = process.env.PORT || 8080
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`)
+})
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLE REJECTION!')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
 })
