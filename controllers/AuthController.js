f449dd8a2c5ce876735daba3925e57835046f752
ducduy0209 <ducduy0209.work@gmail.com>
2022-06-23 12:09:53 +0700

    Commit on 23th June

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 const { promisify } = require('util')
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
@@ -115,7 +116,7 @@ class AuthController {
     const resetURL = `${req.protocol}://${req.get(
       'host'
     )}/api/v1/users/resetPassword/${resetToken}`
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}/n. If you didn't forget your password, please ignore this email!`
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}\n. If you didn't forget your password, please ignore this email!`
 
     try {
       await sendEmail({
@@ -143,7 +144,35 @@ class AuthController {
     })
   })
 
-  resetPassword = catchAsync(async (req, res, next) => {})
+  resetPassword = catchAsync(async (req, res, next) => {
+    // Get user based on token
+    const hasdedToken = crypto
+      .createHash('sha256')
+      .update(req.params.token)
+      .digest('hex')
+
+    const user = await User.findOne({
+      passwordResetToken: hasdedToken,
+      passwordResetExpired: { $gt: Date.now() }
+    })
+    // If token is invalid or has expired, and there is user, set the new password
+    if (!user) {
+      return next(new AppError('Token is invalid or has expired!', 400))
+    }
+
+    user.password = req.body.password
+    user.passwordConfirm = req.body.passwordConfirm
+    user.passwordResetToken = undefined
+    user.passwordResetExpired = undefined
+    await user.save()
+    // Update passwordChangedAt property for the user
+    // Log the user in, send JWT
+    const token = signToken(user._id)
+    res.status(200).json({
+      status: 'success',
+      token
+    })
+  })
 }
 
 module.exports = new AuthController()
